8eae55c8391688c4c70fcdc6162c3653b5961b29
junxtar <aa1870088@naver.com>
2024-02-19 19:39:17 +0900

    Feat: _class 제외 config 추가

--- src/main/java/com/example/mongo/domain/model/BaseEntity.java
+++ src/main/java/com/example/mongo/domain/model/BaseEntity.java
@@ -1,23 +1,22 @@
 package com.example.mongo.domain.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import java.io.Serializable;
+import java.time.ZonedDateTime;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @Getter
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
-public abstract class BaseEntity {
+public abstract class MongoBaseTimeEntity implements Serializable {
 
     @CreatedDate
-    @Column(name = "create_at", updatable = false)
-    private String createdAt;
+    @Field(name = "create_at")
+    private String createdAt = ZonedDateTime.now().toString();
 
     @LastModifiedDate
-    @Column(name="modified_at")
-    private String modifiedAt;
+    @Field(name="modified_at")
+    private String modifiedAt = ZonedDateTime.now().toString();
 }
---
+++ src/main/java/com/example/mongo/domain/model/MongoBaseEntity.java
@@ -0,0 +1,2 @@
+package com.example.mongo.domain.model;public class MongBaseEntity {
+}
